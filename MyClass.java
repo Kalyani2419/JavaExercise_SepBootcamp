9cb74eea6473e71e8a41a6896e505a09da4593fc
Kalyani Lanjewar <87492891+Kalyani2419@users.noreply.github.com>
2021-09-07 11:49:53 +0530

    first commit

---
+++ MyClass.java
@@ -0,0 +1,8 @@
+class MyClass
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello, Road To Code..!");
+        System.out.println("Welcome to JAVA Programming");
+    }
+}
