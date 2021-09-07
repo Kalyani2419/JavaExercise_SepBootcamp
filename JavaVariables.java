668d8afdf22aaeb29fec68e65f86389ce233be32
Kalyani Lanjewar <87492891+Kalyani2419@users.noreply.github.com>
2021-09-07 12:02:11 +0530

    Add files via upload

---
+++ JavaVariables.java
@@ -0,0 +1,34 @@
+class JavaVariables 
+{
+    public static void main(String args[])
+    {
+        byte val = 127; //range= -128 t0 127
+        System.out.println(val);
+
+        short shortVal = 326;
+        System.out.println(shortVal);
+
+        int intVal = -2147483648;
+        System.out.println(intVal);
+
+        long longVal = 8579835;
+        System.out.println(longVal);
+
+        double doubleVal = 3567.999;
+        System.out.println(doubleVal);
+
+        float floatVal = 33.5f;
+        System.out.println(floatVal);
+
+        char charVal = 65;
+        System.out.println(charVal);
+        char charVal2 = 'B';
+        System.out.println(charVal2);
+
+        boolean flag = true;
+        System.out.println(flag);
+
+        String name = "Road To Code";
+        System.out.println(name);
+    }
+}
